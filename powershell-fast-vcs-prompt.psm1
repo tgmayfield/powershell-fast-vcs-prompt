d5637ece7ba3a4dc5ebdaedf7b69d7f64acec34d
Thomas G Mayfield <tgmayfield@gmail.com>
2015-09-04 15:44:23 -0700

    'git branch -v -v' sets the exit code to 255 when there are invalid refs but it's an otherwise valid repository

--- powershell-fast-vcs-prompt.psm1
+++ powershell-fast-vcs-prompt.psm1
@@ -422,8 +422,11 @@ function prompt {
 	function writeGitStatus {
 		$branch = git branch -v -v --color=never 2>&1
 		if ($branch.Exception -ne $null) {
-			$error.clear()
-			return
+			if ($LASTEXITCODE -ne 255)
+			{
+				$error.clear()
+				return
+			}
 		}
 
 		outputMarker " (git "
